68a7d0f112873555e72d8e471ed8b6cc51c22450
Maxim Vanhove <maximvanhove@MacBook-Pro-van-Maxim.local>
2018-05-02 16:49:30 +0200

    Add link to source in console

---
+++ src/console.js
@@ -0,0 +1,7 @@
+window.console.display = text => {
+    console.log(`%c ${text} `, 'background: #3FA9F5; color: #ffffff; border-radius: 2px; padding: 2px;')
+}
+
+window.console.gif = () => {
+    console.log(`%c `, 'background: url(https://media1.tenor.com/images/3d4b1ebeba260716d3205f30143f2674/tenor.gif); background-size: contain; background-repeat: no-repeat; padding: 50px 100px;')
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './console'
+
+console.display('Looking for the source? You can find it here:')
+console.log('https://github.com/MaximVanhove/maximvanhove')
+console.gif()
 
 Vue.config.productionTip = false
 
